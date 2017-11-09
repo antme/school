b3b285f1da1a30f1e2f0cb47f4bd2c0bd3f39373
dylan <251148471@qq.com>
2017-11-09 16:05:52 +0800

    JS UPDATE

--- src/main/webapp/script/common.js
+++ src/main/webapp/script/common.js
@@ -3,9 +3,9 @@
  */
 
 //项目定义
-var ajaxUrlBase = "http://localhost:8080";
+var ajaxUrlBase = "https://queue.dandgedu.com";
 if(window.location.href.indexOf("queuetest.dandgedu.com") != -1){
-    //ajaxUrlBase = "https://queuetest.dandgedu.com";
+    ajaxUrlBase = "https://queuetest.dandgedu.com";
 }
 //通信协议强制切换到https
 var curProtocol = window.location.protocol;
