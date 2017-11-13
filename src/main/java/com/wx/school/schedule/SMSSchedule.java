7333a3d70cb33488a08656cc86df7dad417225c5
dylan <251148471@qq.com>
2017-11-13 16:10:49 +0800

    短信时间设置

--- src/main/java/com/wx/school/schedule/SMSSchedule.java
+++ src/main/java/com/wx/school/schedule/SMSSchedule.java
@@ -18,7 +18,7 @@ public class SMSSchedule {
 
 	private boolean isRuinning = false;
 
-	@Scheduled(cron = "0 0 0/1 * * ?")
+	@Scheduled(cron = "0 0/1 * * * ?")
 	@Async
 	public void run() {
 		if (!isRuinning) {
